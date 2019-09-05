027727c487679b83dfa0aa4bff44a44acf1e8436
Tse, Christopher <ctse@netsuite.com>
2019-09-05 15:59:43 -0500

    Using reference path method

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/// <reference path="util.js">
+
+let a = 5;
+let b = 6;
+
+document.getElementById('sum').textContent = add(a, b);
---
+++ js/util.js
@@ -0,0 +1,8 @@
+/**
+ * Adds two numbers
+ * @param {Number} a 
+ * @param {Number} b 
+ */
+function add(a, b) {
+    return a + b;
+}
